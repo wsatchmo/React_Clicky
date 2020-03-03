bf8b8382bddb6355effdd5972eb2ce424e0e66ec
wsatchmo <wsatcher@outlook.com>
2020-03-02 23:28:01 -0800

    Updates

---
+++ precache-manifest.6e609de8efde35d2cf1d6c9cd4bf819b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3001a5c3352210eb96e97532d64dc88e",
+    "url": "/React_Clicky/index.html"
+  },
+  {
+    "revision": "c56a37ca2d636ca3f155",
+    "url": "/React_Clicky/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8f0a6ecc2086fb0e6d12",
+    "url": "/React_Clicky/static/css/main.8738c6e3.chunk.css"
+  },
+  {
+    "revision": "c56a37ca2d636ca3f155",
+    "url": "/React_Clicky/static/js/2.700912bf.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/React_Clicky/static/js/2.700912bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0a6ecc2086fb0e6d12",
+    "url": "/React_Clicky/static/js/main.73ace353.chunk.js"
+  },
+  {
+    "revision": "23174d1e17a374921f77",
+    "url": "/React_Clicky/static/js/runtime-main.490920f5.js"
+  }
+]);
